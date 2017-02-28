ccfaeb25299a7c4b00147a16dcd5fd3fec6878e5
Renju Radhakrishnan <renju467@gmail.com>
2017-02-28 13:29:51 -0500

    Adds data to elastic search

--- index.js
+++ index.js
@@ -5,16 +5,33 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let app = express();
 let path = require('path');
+let elasticsearch = require('elasticsearch');
+let client = new elasticsearch.Client({
+  host: 'eshost',
+  log: 'trace'
+});
+
+
+client.ping({
+  // ping usually has a 3000ms timeout
+  requestTimeout: 1000
+}, function (error) {
+  if (error) {
+    console.trace('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
+});
+
 
 let getPage = require('summarizer').getPage;
 
 let uri = 'http://drexel.edu/coas/faculty-research/faculty-directory/AkeStacey/';
 let result_set = require('./CSProfessors');
 
-let results = [];
 console.log(`total results :  ${result_set.length}`);
 
-for(let i = 0; i < 2; i++) {
+for(let i = 0; i < result_set.length; i++) {
   getPage(result_set[i].Sources1).then(function (data) {
     let result = (data);
     let currentProfInfo = result_set[i];
@@ -29,12 +46,24 @@ for(let i = 0; i < 2; i++) {
     result.source = [currentProfInfo.Sources1, currentProfInfo.Sources2, currentProfInfo.Sources3, currentProfInfo.Sources4]
     delete result.text;
     delete result.raw;
-    results.push(result);
     console.log(i);
+    let es = {};
+    es.type = 'user';
+    es.index = 'profiles';
+    es.body = result;
+    es.id = i;
+    client.create(es, (err, res) => {
+      if(err) {
+        console.log(err);
+        return
+      }
+      console.log(res);
+    });
   }, console.error);
 }
+
 app.get('/results', (req, res) => {
-  res.send(results);
+  res.send('hello');
 
 })
 let server = app.listen(9000, () => {
