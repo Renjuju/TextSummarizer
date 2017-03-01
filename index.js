314983ee93561cb41ca00d4dea5ccbe71d697469
Renju Radhakrishnan <renju467@gmail.com>
2017-02-28 22:03:12 -0500

    json write

--- index.js
+++ index.js
@@ -6,32 +6,43 @@ let bodyParser = require('body-parser');
 let app = express();
 let path = require('path');
 let elasticsearch = require('elasticsearch');
-let client = new elasticsearch.Client({
-  host: 'eshost',
-  log: 'trace'
-});
+let jsonfile = require('jsonfile')
+let file = '/Users/renju/Developer/node-parser/data.json';
+let data = {
+  somedata: 4,
+  yes: true,
+  last: 'helloworld'
+};
 
 
-client.ping({
-  // ping usually has a 3000ms timeout
-  requestTimeout: 1000
-}, function (error) {
-  if (error) {
-    console.trace('elasticsearch cluster is down!');
-  } else {
-    console.log('All is well');
-  }
-});
+// let client = new elasticsearch.Client({
+//   host: 'eshost',
+//   log: 'trace'
+// });
+
+
+// client.ping({
+//   // ping usually has a 3000ms timeout
+//   requestTimeout: 1000
+// }, function (error) {
+//   if (error) {
+//     console.trace('elasticsearch cluster is down!');
+//   } else {
+//     console.log('All is well');
+//   }
+// });
 
 
 let getPage = require('summarizer').getPage;
 
 let uri = 'http://drexel.edu/coas/faculty-research/faculty-directory/AkeStacey/';
 let result_set = require('./CSProfessors');
-
+let results = [];
 console.log(`total results :  ${result_set.length}`);
 
-for(let i = 0; i < result_set.length; i++) {
+console.log('hello wlrd');
+let i = 0;
+for(i = 0; i < 3; i++) {
   getPage(result_set[i].Sources1).then(function (data) {
     let result = (data);
     let currentProfInfo = result_set[i];
@@ -46,26 +57,31 @@ for(let i = 0; i < result_set.length; i++) {
     result.source = [currentProfInfo.Sources1, currentProfInfo.Sources2, currentProfInfo.Sources3, currentProfInfo.Sources4]
     delete result.text;
     delete result.raw;
+    results.push(result);
     console.log(i);
-    let es = {};
-    es.type = 'user';
-    es.index = 'profiles';
-    es.body = result;
-    es.id = i;
-    client.create(es, (err, res) => {
-      if(err) {
-        console.log(err);
-        return
-      }
-      console.log(res);
-    });
-  }, console.error);
+    // let es = {};
+    // es.type = 'user';
+    // es.index = 'profiles';
+    // es.body = result;
+    // es.id = i;
+    // client.create(es, (err, res) => {
+    //   if(err) {
+    //     console.log(err);
+    //     return
+    //   }
+    //   console.log(res);
+    // });
+  });
 }
 
-app.get('/results', (req, res) => {
-  res.send('hello');
 
+app.get('/results', (req, res) => {
+  jsonfile.writeFile(file, results , (err)=> {
+    console.log(err);
+  });
+  res.send(results);
 })
+
 let server = app.listen(9000, () => {
   	let host = server.address().address;
 	let port = server.address().port;
