8ee184a3e63557c14ba84ae68d62e3d2382b74a0
Renju Radhakrishnan <renju467@gmail.com>
2017-02-28 22:33:01 -0500

    catching errors

--- index.js
+++ index.js
@@ -35,43 +35,45 @@ let data = {
 
 let getPage = require('summarizer').getPage;
 
-let uri = 'http://drexel.edu/coas/faculty-research/faculty-directory/AkeStacey/';
 let result_set = require('./CSProfessors');
 let results = [];
 console.log(`total results :  ${result_set.length}`);
 
-console.log('hello wlrd');
-let i = 0;
-for(i = 0; i < 3; i++) {
-  getPage(result_set[i].Sources1).then(function (data) {
-    let result = (data);
-    let currentProfInfo = result_set[i];
 
-    result.name = currentProfInfo.Name;
-    result.university = currentProfInfo.University;
-    result.JoinYear = currentProfInfo.JoinYear;
-    result.rank = currentProfInfo.rank;
-    result.subfield = currentProfInfo.Subfield;
-    result.bachelors = currentProfInfo.Bachelors;
-    result.masters = currentProfInfo.Masters;
-    result.source = [currentProfInfo.Sources1, currentProfInfo.Sources2, currentProfInfo.Sources3, currentProfInfo.Sources4]
-    delete result.text;
-    delete result.raw;
-    results.push(result);
-    console.log(i);
-    // let es = {};
-    // es.type = 'user';
-    // es.index = 'profiles';
-    // es.body = result;
-    // es.id = i;
-    // client.create(es, (err, res) => {
-    //   if(err) {
-    //     console.log(err);
-    //     return
-    //   }
-    //   console.log(res);
-    // });
-  });
+for(let i = 0; i < result_set.length; i++) {
+  try {
+    getPage(result_set[i].Sources1).then(function (data) {
+      let result = (data);
+      let currentProfInfo = result_set[i];
+
+      result.name = currentProfInfo.Name;
+      result.university = currentProfInfo.University;
+      result.JoinYear = currentProfInfo.JoinYear;
+      result.rank = currentProfInfo.rank;
+      result.subfield = currentProfInfo.Subfield;
+      result.bachelors = currentProfInfo.Bachelors;
+      result.masters = currentProfInfo.Masters;
+      result.source = [currentProfInfo.Sources1, currentProfInfo.Sources2, currentProfInfo.Sources3, currentProfInfo.Sources4]
+      delete result.text;
+      delete result.raw;
+      results.push(result);
+      console.log(i);
+      // let es = {};
+      // es.type = 'user';
+      // es.index = 'profiles';
+      // es.body = result;
+      // es.id = i;
+      // client.create(es, (err, res) => {
+      //   if(err) {
+      //     console.log(err);
+      //     return
+      //   }
+      //   console.log(res);
+      // });
+    });
+  }catch(e) {
+    console.log(e);
+  }
 }
 
 
